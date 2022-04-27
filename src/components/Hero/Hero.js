9c640eeec56680670e37551cd55f1008f2240b1d
Antonio94709 <exilhekkiekassongo6@gmail.com>
2022-04-28 01:14:53 +0200

    First commit

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
+import Button from '../../styles/GlobalComponents/Button';
+import { LeftSection } from './HeroStyles';
+
+const Hero = (props) => (
+  <>
+    <Section row nopadding>
+      <LeftSection>
+        <SectionTitle main center>
+          Good Day. I am Exil Kassongo <br />
+          Front-end developer
+        </SectionTitle>
+        <SectionText>
+        I am a front end developer thats actively learning and using new technologies that improve experience for developers and users. I have no bounds on what I should learn and this is evitend by my espirasion to eventually become a full-stack dev. 
+        </SectionText>
+        {/* <Button onClick={props.handleClick}>Learn More</Button> */}
+      </LeftSection>
+    </Section>
+  </>
+);
+
+export default Hero;
---
+++ src/components/Technologies/Technologies.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import { DiFirebase, DiReact, DiZend } from 'react-icons/di';
+import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
+import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
+
+const Technologies = () =>  (
+  <Section id="tech">
+    <SectionDivider divider />
+    <SectionTitle>Technologies</SectionTitle>
+    <SectionText>
+      I've worked with a range a technologies in the web development world.
+      From Back-end To Design
+    </SectionText>
+    <List>
+      <ListItem>
+        <picture>
+          <DiReact size="3rem" />
+        </picture>
+        <ListContainer>
+          <ListTitle>Front-End</ListTitle>
+          <ListParagraph>
+            Experiece with <br />
+           <ul style={{}}>
+             <li>React.js</li>
+           <li>Next.js</li>
+           <li>Recoil</li>
+           <li>Context</li>  
+           <li>HTML</li> 
+           </ul>
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <picture>
+          <DiFirebase size="3rem" />
+        </picture>
+        <ListContainer>
+          <ListTitle>Back-End</ListTitle>
+          <ListParagraph>
+            Experience with <br />
+           <ul>
+             <li>Graph QL</li>
+             <li>Graph CMS</li>
+             <li>Express.js</li>
+             <li>Node.js</li>
+             <li>Mongo.db</li>
+             <li>Mysql</li>
+             <li>SQLite</li>
+           </ul>
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <picture>
+          <DiZend size="3rem" />
+        </picture>
+        <ListContainer>
+          <ListTitle>UI/UX</ListTitle>
+          <ListParagraph>
+            Experience with <br />
+            <li>Ionic</li>
+           <li>Tailwind.css</li>
+           <li>CSS</li>
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+    </List>
+    <SectionDivider colorAlt />
+  </Section>
+);
+
+export default Technologies;
